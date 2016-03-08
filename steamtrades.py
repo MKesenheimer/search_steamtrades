3d05082cb84ddfd63cdd9738777b65e7d88a3579
Mattef <mattoef@yahoo.de>
2016-03-08 21:12:49 +0100

    Kleine Änderungen an steamtrades.py

--- steamtrades.py
+++ steamtrades.py
@@ -16,15 +16,38 @@ hlines = [line.rstrip('\n') for line in open("hgames.list")]
 
 browser = webdriver.Firefox()
 browser.get("http://www.steamgifts.com/trades")
+try:
+    input("Please login through Steam and press Enter to continue...")
+except SyntaxError:
+    pass
+browser.get("http://www.steamgifts.com/trades")
 results = "No results were found."
 
-for j in range(0,len(hlines)):
+# sort by games you have [hsort] or sort by the games you want [wsort]
+sort = "wsort"
+
+if sort is "hsort":
+    for j in range(0,len(hlines)):
+        for i in range(0, len(wlines)):
+            browser.find_element_by_class_name("trade__search-have").clear()
+            browser.find_element_by_class_name("trade__search-have").send_keys(hlines[j])
+            browser.find_element_by_class_name("trade__search-want").clear()
+            browser.find_element_by_class_name("trade__search-want").send_keys(wlines[i])
+            browser.find_element_by_class_name("trade__search-submit").click()
+            results = browser.find_element_by_class_name("pagination__results").text
+            if results not in "No results were found.":
+                print "Found results for [H]", hlines[j],"[W]",wlines[i],":",results
+        print ""
+
+if sort is "wsort":
     for i in range(0, len(wlines)):
-        browser.find_element_by_class_name("trade__search-have").clear()
-        browser.find_element_by_class_name("trade__search-have").send_keys(hlines[j])
-        browser.find_element_by_class_name("trade__search-want").clear()
-        browser.find_element_by_class_name("trade__search-want").send_keys(wlines[i])
-        browser.find_element_by_class_name("trade__search-submit").click()
-        results = browser.find_element_by_class_name("pagination__results").text
-        if results not in "No results were found.":
-            print "Found result for [H]", hlines[j],"[W]",wlines[i]
+        for j in range(0,len(hlines)):
+            browser.find_element_by_class_name("trade__search-have").clear()
+            browser.find_element_by_class_name("trade__search-have").send_keys(hlines[j])
+            browser.find_element_by_class_name("trade__search-want").clear()
+            browser.find_element_by_class_name("trade__search-want").send_keys(wlines[i])
+            browser.find_element_by_class_name("trade__search-submit").click()
+            results = browser.find_element_by_class_name("pagination__results").text
+            if results not in "No results were found.":
+                print "Found results for [H]", hlines[j],"[W]",wlines[i],":",results
+        print ""
