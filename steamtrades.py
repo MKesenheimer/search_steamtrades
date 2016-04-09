9768ea7a0f8a0b562be2f33f7071c2b4b61f6146
Mattef <mattoef@yahoo.de>
2016-04-09 15:45:23 +0200

    Scripts now sorts out all locked links

--- steamtrades.py
+++ steamtrades.py
@@ -15,39 +15,98 @@ wlines = [line.rstrip('\n') for line in open("wgames.list")]
 hlines = [line.rstrip('\n') for line in open("hgames.list")]
 
 browser = webdriver.Firefox()
-browser.get("http://www.steamgifts.com/trades")
-try:
-    input("Please login through Steam and press Enter to continue...")
-except SyntaxError:
-    pass
+#browser.get("http://www.steamgifts.com/trades")
+#try:
+#    input("Please login through Steam and press Enter to continue...")
+#except SyntaxError:
+#    pass
 browser.get("http://www.steamgifts.com/trades")
 results = "No results were found."
 
-# sort by games you have [hsort] or sort by the games you want [wsort]
-sort = "wsort"
+# don't show or show the locked links
+#showbad = "yes"
+showbad = "no"
 
-if sort is "hsort":
-    for j in range(0,len(hlines)):
-        for i in range(0, len(wlines)):
-            browser.find_element_by_class_name("trade__search-have").clear()
-            browser.find_element_by_class_name("trade__search-have").send_keys(hlines[j])
-            browser.find_element_by_class_name("trade__search-want").clear()
-            browser.find_element_by_class_name("trade__search-want").send_keys(wlines[i])
-            browser.find_element_by_class_name("trade__search-submit").click()
-            results = browser.find_element_by_class_name("pagination__results").text
-            if results not in "No results were found.":
-                print "Found results for [H]", hlines[j],"[W]",wlines[i],":",results
-        print ""
+# colors
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-if sort is "wsort":
-    for i in range(0, len(wlines)):
+for i in range(0, len(wlines)):
+    
+    # first check if the game exists:
+    browser.find_element_by_class_name("trade__search-have").clear()
+    browser.find_element_by_class_name("trade__search-want").clear()
+    browser.find_element_by_class_name("trade__search-want").send_keys(wlines[i])
+    browser.find_element_by_class_name("trade__search-submit").click()
+    results = browser.find_element_by_class_name("pagination__results").text
+    
+    # if the search was succesful, continue
+    if results not in "No results were found.":
         for j in range(0,len(hlines)):
+            
+            # then check every combination
             browser.find_element_by_class_name("trade__search-have").clear()
             browser.find_element_by_class_name("trade__search-have").send_keys(hlines[j])
             browser.find_element_by_class_name("trade__search-want").clear()
             browser.find_element_by_class_name("trade__search-want").send_keys(wlines[i])
             browser.find_element_by_class_name("trade__search-submit").click()
-            results = browser.find_element_by_class_name("pagination__results").text
+            results  = browser.find_element_by_class_name("pagination__results").text
+            
+            # if a combination gives results, print informations
             if results not in "No results were found.":
-                print "Found results for [H]", hlines[j],"[W]",wlines[i],":",results
-        print ""
+                
+                # reset the counters and the flags
+                resultsfound = "no"
+                ngood = 0
+                nbad  = 0
+                glinks = []
+                blinks = []
+                
+                # go through all entries
+                for entries in browser.find_elements_by_xpath("//*[@class='table__row-outer-wrap']"):
+                    
+                    # look if the entry is faded (locked), if yes don't print it's informations
+                    if not entries.find_elements_by_xpath(".//*[@class='table__row-inner-wrap is-faded']"):
+                        resultsfound = "yes"
+                        
+                        # save the links
+                        for info in entries.find_elements_by_xpath(".//*[@class='table__column__heading']"):
+                            glinks.append(info.get_attribute("href"))
+                            ngood += 1
+                    else:
+                        if showbad is "yes":
+                            resultsfound = "yes"
+                        
+                        for info in entries.find_elements_by_xpath(".//*[@class='table__column__heading']"):
+                            blinks.append(info.get_attribute("href"))
+                            nbad += 1
+                
+                # now print the links if results found
+                if resultsfound is "yes":
+                    print bcolors.OKBLUE+"Found results for [H]",bcolors.HEADER+hlines[j],bcolors.OKBLUE+"[W]",bcolors.HEADER+wlines[i],bcolors.OKBLUE+":",results,bcolors.ENDC
+                    if showbad is "yes":
+                        print "-> Good entries:"
+                    for k in range(0,ngood):
+                        print "  ",glinks[k]
+                
+                    if showbad is "yes":
+                        print "-> Bad entries:"
+                        for k in range(0,nbad):
+                            print "  ",blinks[k]
+
+                # stop for debugging
+                #sys.exit(1)
+    print ""
+
+
+
+
+
+
