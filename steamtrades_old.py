a7456df205cbf234b1cb2235cc1e42fc1365ec6c
Mattef <mattoef@yahoo.de>
2017-02-15 11:27:34 +0100

    Changes on steamtrades.com made some changes necessary.

--- steamtrades_old.py
+++ steamtrades_old.py
@@ -10,7 +10,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from pprint import pprint
 
 import selenium, time
 import sys
@@ -26,7 +25,7 @@ browser.get("https://www.steamtrades.com/trades")
 #    input("Please login through Steam and press Enter to continue...")
 #except SyntaxError:
 #    pass
-#browser.get("https://www.steamtrades.com/trades")
+#browser.get("http://www.steamgifts.com/trades")
 results = "No results were found."
 
 # don't show or show the locked links
@@ -47,14 +46,13 @@ class bcolors:
 for i in range(0, len(wlines)):
     
     # first check if the game exists:
-    url = "https://www.steamtrades.com/trades/search?"
-    #link = url+"have="+hlines[0]+"&want="+wlines[i]
-    link = url+"want="+wlines[i]
-    #print link
-    browser.get(link)
-    results = browser.find_element_by_class_name("pagination_results").text
-    # reset the browser (sometimes an error occured when the page is still filled)
-    browser.get("https://www.steamtrades.com/trades")
+    browser.find_element_by_class_name("trade__search-have").clear()
+    browser.find_element_by_class_name("trade__search-want").clear()
+    browser.find_element_by_class_name("trade__search-want").send_keys(wlines[i])
+    browser.find_element_by_class_name("trade__search-submit").click()
+    results = browser.find_element_by_class_name("pagination__results").text
+    # reset the browser (sometimes an error occured when the page was still filled
+    browser.get("http://www.steamgifts.com/trades")
     
     # if the search was succesful, continue
     if results not in "No results were found.":
@@ -64,10 +62,12 @@ for i in range(0, len(wlines)):
             results = "No results were found."
             
             # then check every combination
-            url = "https://www.steamtrades.com/trades/search?"
-            link = url+"have="+hlines[j]+"&want="+wlines[i]
-            browser.get(link)
-            results = browser.find_element_by_class_name("pagination_results").text
+            browser.find_element_by_class_name("trade__search-have").clear()
+            browser.find_element_by_class_name("trade__search-have").send_keys(hlines[j])
+            browser.find_element_by_class_name("trade__search-want").clear()
+            browser.find_element_by_class_name("trade__search-want").send_keys(wlines[i])
+            browser.find_element_by_class_name("trade__search-submit").click()
+            results = browser.find_element_by_class_name("pagination__results").text
             #print results
             # if a combination gives results, print informations
             if results not in "No results were found.":
@@ -80,24 +80,21 @@ for i in range(0, len(wlines)):
                 blinks = []
                 
                 # go through all entries
-                for entries in browser.find_elements_by_xpath("//*[@class='row_outer_wrap']"):
+                for entries in browser.find_elements_by_xpath("//*[@class='table__row-outer-wrap']"):
                     
                     # look if the entry is faded (locked), if yes don't print it's informations
-                    if not entries.find_elements_by_xpath("//*[@class='row_inner_wrap is_faded']"):
+                    if not entries.find_elements_by_xpath(".//*[@class='table__row-inner-wrap is-faded']"):
                         resultsfound = "yes"
                         
                         # save the links
-                        for info in entries.find_elements_by_xpath("//*[@class='row_inner_wrap']//*[@class='column_flex']/h3/a"):
-                            #print info.text
-                            #print info.get_attribute("href")
+                        for info in entries.find_elements_by_xpath(".//*[@class='table__column__heading']"):
                             glinks.append(info.get_attribute("href"))
                             ngood += 1
-                            
                     else:
                         if showbad is "yes":
                             resultsfound = "yes"
                         
-                        for info in entries.find_elements_by_xpath("//*[@class='row_inner_wrap']//*[@class='column_flex']/h3/a"):
+                        for info in entries.find_elements_by_xpath(".//*[@class='table__column__heading']"):
                             blinks.append(info.get_attribute("href"))
                             nbad += 1
                 
@@ -117,3 +114,18 @@ for i in range(0, len(wlines)):
                 # stop for debugging
                 #sys.exit(1)
     print ""
+
+
+
+# NOTES: instead of using the lengthy expressions with browser.find_element_by_class_name
+# a url-based search could be used. For that, the search is encoded in an url query of the form:
+# https://www.steamgifts.com/trades/search?have=name1&want=name2
+#
+# The code could look like that:
+#   url = "https://www.steamgifts.com/trades/search?"
+#   link = url+"have="+hlines[j]+"want="+wlines[i]
+#   link = link.replace(" ","+")
+#   print link
+#   browser.get(link)
+#
+# Problem: Selenium ignores every '+' character in the url, which makes this method useless for now.
